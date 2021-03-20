98dbeedb021e06336ab0fe2506bed5fedee63bb5
Enia Munteanu <75390874+Moonia9@users.noreply.github.com>
2021-03-20 15:35:21 +0100

    updated exercise-demo.js added in class

--- week-1/Homework/demo-ternaryOperator.js
+++ week-1/Homework/demo-ternaryOperator.js
@@ -1,1 +1,8 @@
-5 < 10
+5 < 10 ? console.log("5 is less than 10") : console.log("5 is not less than 10");
+
+let a=10, b=5
+a < b /* && a!=b */ ? console.log(`${a} is less than ${b}`) : console.log(`${a} is not less than ${b}`);
+
+//Syntax
+//condition ? true : false
+//condition ? 'if true --> return this' : 'if not true(false) --> return this'
