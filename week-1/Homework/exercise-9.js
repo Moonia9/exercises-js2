29a71e294b9906dcfa8fb68180763c726f5ed4f7
Enia Munteanu <75390874+Moonia9@users.noreply.github.com>
2021-03-20 20:35:42 +0100

    updated exercise-9.js/Homework

--- week-1/Homework/exercise-9.js
+++ week-1/Homework/exercise-9.js
@@ -15,16 +15,6 @@ var product = {
   stock:
 }
 id is a unique number that identifies each product
-
-3. Create a function addToShoppingCart to add a product to the shopping cart list giving the product id,
-the function will add the product to the selectedProduct list, and add the price to the totalPrice
-
-4. Create the function removeFrom ShoppingCart to remove a product that a client does not like anymore
-
-5. Create the function shop, the function will empty the list and set 0 in the totalPrice of the shopping cart
-In addition will substract 1 in the product stock of bought products
-
-6. If there is not enough stock, the product cannot be added to the shopping cart
 */
 
 var products = [];
@@ -41,26 +31,80 @@ var product2 = {
   price: 9.99,
   stock: 2
 };
+var product3 = {
+  id: 3,
+  name: "RedBull",
+  price: 2.35,
+  stock: 50
+};
+var product4 = {
+  id: 4,
+  name: "Pufuleti",
+  price: 0.35,
+  stock: 20
+};
 
 products.push(product1);
 products.push(product2);
+products.push(product3);
+products.push(product4);
 
 var shoppingCart = {
   totalPrice: 0,
   selectedProducts: []
 };
 
+// 3. Create a function addToShoppingCart to add a product to the shopping cart list giving the product id,
+// the function will add the product to the selectedProduct list, and add the price to the totalPrice
 function addToShoppingCart(id){
-
+  let selectedProduct = products.find(p => p.id === id)
+shoppingCart.selectedProducts.push(selectedProduct)
+shoppingCart.totalPrice = shoppingCart.totalPrice + selectedProduct.price
+//or shoppingCart.totalPrice += selectedProducts.price
 }
 
+// 4. Create the function removeFrom ShoppingCart to remove a product that a client does not like anymore
 function removeFromShoppingCart(id){
-
+  let selectedProduct = products.find(p => p.id === id)
+  shoppingCart.selectedProducts = shoppingCart.selectedProducts.filter(p => p.id !== id)
+  shoppingCart.totalPrice = shoppingCart.totalPrice - selectedProduct.price
 }
 
+// 5. Create the function shop, the function will empty the list and set 0 in the totalPrice of the shopping cart
+// In addition will substract 1 in the product stock of bought products
 function shop(){
-
+  shoppingCart.selectedProducts.forEach(sp => {
+    products.forEach(p => {
+      if(p.id === sp.id){
+        if(p.stock > 0){
+          p.stock = p.stock - 1 //or p.stock --
+        }
+        if(p.stock === 0) {
+          console.log(`This product is not available for this quantity`)
+          return
+        }
+      }
+    })
+  })
+  shoppingCart.selectedProducts = [];
+  shoppingCart.totalPrice = 0;
 }
+
+// 6. If there is not enough stock, the product cannot be added to the shopping cart
+function addToShoppingCart(id){
+  let selectedProduct = products.find(p => p.id === id)
+  if(selectedProduct.stock > 0){
+    shoppingCart.selectedProducts.push(selectedProduct)
+    shoppingCart.totalPrice = shoppingCart.totalPrice + selectedProduct.price
+  }
+
+//or shoppingCart.totalPrice += selectedProducts.price
+}
+
+
+
+
+
 
 //results
 addToShoppingCart(1);
