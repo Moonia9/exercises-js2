cf9d059377efd9c0a87f3407dd617d1ad60aeb43
Enia Munteanu <75390874+Moonia9@users.noreply.github.com>
2021-03-26 23:24:16 +0100

    updated exercise-1/Homework/w2

--- week-2/Homework/1-exercises/exercises.js
+++ week-2/Homework/1-exercises/exercises.js
@@ -14,7 +14,16 @@
  * </div>
  */
 function exerciseOne(arrayOfPeople) {
-  let content = document.querySelector("#content");
+  const content = document.querySelector("#content");
+  arrayOfPeople.forEach(value => {
+    const firstHeading = document.createElement("h1");
+    firstHeading.textContent = value.name;
+    content.appendChild(firstHeading);
+
+    const secondHeading = document.createElement("h2");
+    secondHeading.textContent = value.job;
+    content.appendChild(secondHeading);
+  });
 }
 
 /**
@@ -25,7 +34,12 @@ function exerciseOne(arrayOfPeople) {
  *
  */
 function exerciseTwo(shopping) {
-  //Write your code in here
+  const content = document.querySelector("#content");
+  shopping.forEach(product => {
+    const ul = document.createElement("ul");
+    ul.textContent = product;
+    content.append(product);
+  });
 }
 
 /**
@@ -50,15 +64,59 @@ function exerciseTwo(shopping) {
     ];
 
     Iterate through the array of books.
-    - For each book, create a <p> element with the book title and author and append it to the page.
-    - Use a <ul> and <li> to display the books.
-    - Add an <img> to each book that links to a URL of the book cover.
-    - Change the style of the book depending on whether you have read it (green) or not (red).
+    1.- For each book, create a <p> element with the book title and author and append it to the page.
+    2.- Use a <ul> and <li> to display the books.
+    3.- Add an <img> to each book that links to a URL of the book cover.
+    4.- Change the style of the book depending on whether you have read it (green) or not (red).
 
     The end result should look something like this: https://hyf-js2-week1-makeme-ex1-demo.herokuapp.com/
 **/
 function exerciseThree(books) {
-  //Write your code in here
+  //3.
+  let firstBook = books[0];
+  firstBook.img = "https://miro.medium.com/max/1200/1*Qo27inBKBKY4Q4Pgk5KkbQ.png";
+  let secondBook = books[1];
+  secondBook.img = "https://images-na.ssl-images-amazon.com/images/I/71HMyqG6MRL.jpg";
+  let thirdBook = books[2];
+  thirdBook.img = 'https://m.media-amazon.com/images/I/41uPjEenkFL._SX260_.jpg';
+
+  //1.
+  let content = document.querySelector("#content");
+  const h1 = document.createElement("h1");
+  h1.textContent = "Book list";
+  content.appendChild(h1);
+
+  const flexboxContent = document.createElement("div");
+  content.appendChild(flexboxContent);
+  //2.
+  const ul = document.createElement("ul");
+  flexboxContent.appendChild(ul);
+  ul.style.display = "flex";
+  ul.style.justifyContent = "space-around";
+
+  //1.
+  books.forEach(item => {
+    const li = document.createElement("li");
+    const p = document.createElement("p");
+    p.textContent = item.title + " - " + item.author; 
+    li.appendChild(p);
+    const img = document.createElement("img");
+    //style
+    img.style.height = "260px";
+    img.style.width = "200px";
+    img.style.margin = "10px";
+    img.src = item.img;
+    li.append(img);
+   //4.
+  //  if(item.alreadyRead === true){
+  //   item.backgroundColor = "green";
+  // } else {
+  //   item.backgroundColor = "red";
+  // }
+   li.style.backgroundColor = item.alreadyRead ? "green" : "red";
+   ul.appendChild(li);
+  
+});
 }
 
 //
